02cb252adf0fa28cb4fb57d2fb765de5d2f2d74d
Zeepnely <kamheater.55@gmail.com>
2024-12-18 10:29:46 -0800

    added running

--- player/running_state.gd
+++ player/running_state.gd
@@ -1,4 +1,4 @@
-class_name shipplayerwalkingState
+class_name runningState
 extends PlayerMovementState
 
 @onready var player = get_parent().get_parent()
@@ -12,12 +12,12 @@ func update(delta):
 	var input_dir = Input.get_vector("a", "d", "w", "s")
 	var direction = (player.body.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	player.velocity.x = lerp(player.velocity.x, direction.x * 4, delta * 20)
-	player.velocity.z = lerp(player.velocity.z, direction.z * 4, delta * 20)
+	player.velocity.x = lerp(player.velocity.x, direction.x * 6, delta * 20)
+	player.velocity.z = lerp(player.velocity.z, direction.z * 6, delta * 20)
 	
 	if not direction:
-		player.velocity.x = lerp(player.velocity.x, direction.x * 4, delta * 10)
-		player.velocity.z = lerp(player.velocity.z, direction.z * 4, delta * 10)
+		player.velocity.x = lerp(player.velocity.x, direction.x * 6, delta * 10)
+		player.velocity.z = lerp(player.velocity.z, direction.z * 6, delta * 10)
 	
 	
 	
@@ -27,8 +27,9 @@ func update(delta):
 				
 	if player.velocity.length() < .001:
 		transition.emit("idleShip")
-	if Input.is_action_pressed("shift"):
-		transition.emit("runningState")
+	
+	if !Input.is_action_pressed("shift"):
+		transition.emit("shipplayerwalkingState")
 		
 	if !player.is_on_floor():
 		if player.velocity.y <= 0:
